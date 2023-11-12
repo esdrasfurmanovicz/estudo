d4abb2205fc810fc8a16b6b4523b729b1d5bd8f0
Esdras Furmanovicz <128192976+esdrasfurmanovicz@users.noreply.github.com>
2023-11-12 13:48:53 -0300

    muitos botões

--- prototipo/js/principal.js
+++ prototipo/js/principal.js
@@ -18,6 +18,18 @@ window.addEventListener('load', function() {
 }); 
 */
 /* add carro */
+let checkNoti = document.querySelector('#checknoti')
+checkNoti.addEventListener('click', function(){
+    let avBlock = document.querySelector('.avBlock')
+    console.log(avBlock)
+    if (checkNoti.checked) {
+        avBlock.style.display = 'block'
+    } else {
+        avBlock.style.display = 'none'
+    }
+})
+
+
 const pesqu = document.getElementById('form')
 pesqu.addEventListener("submit", function (event) {
     event.preventDefault()
@@ -27,14 +39,38 @@ pesqu.addEventListener("submit", function (event) {
     if (placa === 'BRA2E19' || placa === 'bra2e19') {
         user.style.display = 'block'
         pingo.style.display = 'block'
-    } else{
+        fundo.style.display = 'none'
+    } else {
         alert('Placa inválida!')
     }
 })
 /* Navegar até tal parte da pagina */
 function navigateToDestination(destino) {
     let i = document.querySelector(destino)
-    i.scrollIntoView({behavior: "smooth"});
+    i.scrollIntoView({ behavior: "smooth" });
 }
 
-
+let more1 = document.querySelector('#more1')
+let more2 = document.querySelector('#more2')
+let veiLac1 = document.querySelector('#veiLac-1')
+let veiLac2 = document.querySelector('#veiLac-2')
+more1.addEventListener('click', function () {
+    let mais1 = document.querySelector('.mais1')
+    if (more1.checked) {
+        mais1.style.display = 'block'
+        veiLac1.style.boxShadow = '0px 5px 10px rgba(0, 0, 0, 0.429)'
+    } else {
+        mais1.style.display = 'none'
+        veiLac1.style.boxShadow = 'none'
+    }
+})
+more2.addEventListener('click', function () {
+    let mais2 = document.querySelector('.mais2')
+    if (more2.checked) {
+        mais2.style.display = 'block'
+        veiLac2.style.boxShadow = '0px 5px 10px rgba(0, 0, 0, 0.429)'
+    } else {
+        mais2.style.display = 'none'
+        veiLac2.style.boxShadow = 'none'
+    }
+})
