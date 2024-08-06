ab86bfa1fbaa6f47694660327982d83123bf0952
Esdras Furmanovicz <128192976+esdrasfurmanovicz@users.noreply.github.com>
2024-08-06 10:20:37 -0300

    a

---
+++ a/jao/lista.py
@@ -0,0 +1,15 @@
+lista:
+    compras, formandos, filmes
+    De compras eu faria por importancia
+    Formandos, lista alfabetica
+    Filmes, lista alfabetica ou notas imdb
+Fila:
+    pessoas, formandos, carros
+    carros, pode ser uma fila por porte, cor, placa...
+    formandos, fila por ordem alfabetica
+    pessoas, fila por ordem de idade
+Pilha:
+    dinheiro, pratos, livros
+    Livros, alfabetico (nome e categoria), cor, numero
+    Pratos, tamanho, cor, material
+    Dinheiro, valor da nota
---
+++ a/jao/pds/quadrado.py
@@ -0,0 +1,12 @@
+import math
+
+def calcular_area_circulo(raio):
+
+  area = math.pi * raio**2
+  return area
+
+raio = float(input("Digite o valor do raio do círculo: "))
+
+area = calcular_area_circulo(raio)
+
+print("A área do círculo é:", area)
